ee793e4ff9193de2d2e62aa676fe804518919453
PiotrPleska <piotrekeska22@gmail.com>
2023-03-15 23:29:39 +0100

    icon to app

--- src/main/java/krypto/GUI/ApplicationGUI.java
+++ src/main/java/krypto/GUI/ApplicationGUI.java
@@ -4,8 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.util.Objects;
 
 public class ApplicationGUI extends Application {
@@ -13,6 +15,8 @@ public class ApplicationGUI extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainScreen.fxml")));
         Scene scene = new Scene(root);
+        Image icon = new Image(getClass().getResourceAsStream("encryption-culture-mydiamo.jpg"));
+        stage.getIcons().add(icon);
         stage.setTitle("Projekt Kryptografia");
         stage.setScene(scene);
         stage.show();
