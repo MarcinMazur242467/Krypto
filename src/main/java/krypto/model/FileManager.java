e874fcc4d17a65bbbafc3f03936f778d2dd26d55
PiotrPleska <piotrekeska22@gmail.com>
2023-03-22 23:08:05 +0100

    reading pdf files. Saving doesnt work

--- src/main/java/krypto/model/FileManager.java
+++ src/main/java/krypto/model/FileManager.java
@@ -1,12 +1,15 @@
 package krypto.model;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.text.PDFTextStripper;
+
 
 public class FileManager {
     private final File fileName;
@@ -15,15 +18,68 @@ public class FileManager {
         this.fileName = fileName;
     }
 
+
     public byte[] read() throws IOException {
-        Path path = fileName.toPath();
-        return Files.readAllBytes(path);
+        byte[] buffer;
+        if(fileName.toString().contains(".pdf")){
+            PDDocument document = PDDocument.load(new File(fileName.getPath()));
+            PDFTextStripper stripper = new PDFTextStripper();
+            String text = stripper.getText(document);
+            buffer = text.getBytes();
+            document.close();
+        }else {
+            File file = new File(fileName.getPath());
+            int fileSize = (int) file.length();
+            buffer = new byte[fileSize];
+            FileInputStream inputStream = new FileInputStream(file);
+            inputStream.read(buffer);
+            inputStream.close();
+
+        }
+        return buffer;
     }
 
+
     public void write(String s) throws IOException {
+        if(fileName.toString().contains(".pdf")){
+            PDDocument document = new PDDocument();
+            PDPage page = new PDPage();
+            document.addPage(page);
+            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+            contentStream.beginText();
+            float fontSize = 12;
+            final int MAX_LENGTH = 50;
+            List<String> result = new ArrayList<>();
+            s = s.replaceAll("\n", " ");
+            String[] words = s.split("\\s+");
+            String current = "";
+            for (String word : words) {
+                if ((current + " " + word).length() <= MAX_LENGTH) {
+                    current += " " + word;
+                } else {
+                    result.add(current.trim());
+                    current = word;
+                }
+            }
+            if (!current.isEmpty()) {
+                result.add(current.trim());
+            }
+            contentStream.setFont(PDType1Font.TIMES_ROMAN,fontSize);
+            contentStream.moveTextPositionByAmount(50, 700);
+            for (String value : result) {
+                contentStream.showText(value);
+                contentStream.newLine();
+            }
+            contentStream.endText();
+            contentStream.close();
+            document.save(fileName);
+            document.close();
+        }
+        else {
             BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
             writer.write(s);
             writer.close();
+        }
     }
 }
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module krypto.krypto {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.controlsfx.controls;
+    requires org.apache.pdfbox;
 
 
     opens krypto.GUI to javafx.fxml;
