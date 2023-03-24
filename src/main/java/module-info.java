c179e44895705a60c5d66d7e05070547c15ab4c4
PiotrPleska <piotrekeska22@gmail.com>
2023-03-24 20:06:17 +0100

    trying to create a jar

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,6 @@ module krypto.krypto {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.controlsfx.controls;
-    requires org.apache.pdfbox;
-    requires itextpdf;
-    requires org.apache.commons.codec;
-
-
     opens krypto.GUI to javafx.fxml;
     exports krypto.GUI;
 }
